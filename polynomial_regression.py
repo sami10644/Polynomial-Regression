2ed2418b44a02d256598c304181ab2aab5d82067
sami10644 <61244442+sami10644@users.noreply.github.com>
2021-04-03 23:09:41 +0600

    Update polynomial_regression.py

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -28,6 +28,8 @@ import pandas as pd
 
 
 
+
+
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
@@ -74,5 +76,4 @@ plt.show()
 # Predicting a new result with Linear Regression
 lin_reg.predict([[6.5]])
 
-# Predicting a new result with Polynomial Regression
 lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
